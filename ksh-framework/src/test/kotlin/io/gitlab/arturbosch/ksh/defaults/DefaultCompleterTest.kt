b9276e0b8c9d76cb66fab898aa0cf966e5d09ec5
Artur Bosch <arturbosch@gmx.de>
2018-12-29 22:20:27 +0100

    Write tests for the completer

---
+++ ksh-framework/src/test/kotlin/io/gitlab/arturbosch/ksh/defaults/DefaultCompleterTest.kt
@@ -0,0 +1,81 @@
+package io.gitlab.arturbosch.ksh.defaults
+
+import assertk.assertions.containsAll
+import io.gitlab.arturbosch.ksh.Conversions
+import io.gitlab.arturbosch.ksh.Hello
+import io.gitlab.arturbosch.ksh.api.ShellClass
+import io.gitlab.arturbosch.ksh.testContext
+import org.jline.reader.Candidate
+import org.jline.reader.Completer
+import org.jline.reader.impl.DefaultParser
+import org.junit.Test
+
+/**
+ * @author Artur Bosch
+ */
+class DefaultCompleterTest {
+
+    class DefaultCompleterWrapper(commands: List<ShellClass>) {
+        private val completer: Completer = DefaultCompleter(commands)
+        private val parser = DefaultParser()
+        private val context = testContext()
+
+        fun complete(currentLine: String): List<String> {
+            val line = parser.parse(currentLine, (currentLine.length - 1).takeIf { it >= 0 } ?: 0)
+            val candidates = mutableListOf<Candidate>()
+            completer.complete(context.reader, line, candidates)
+            return candidates.map { it.value() }
+        }
+    }
+
+    private val commandsUnderTest = listOf(Hello(), Conversions())
+    private val completer = DefaultCompleterWrapper(commandsUnderTest)
+
+    @Test
+    fun `completion of all shell classes if nothing is entered`() {
+        val candidates = completer.complete("")
+        assertk.assert(candidates).containsAll("hello", "conversions")
+    }
+
+    @Test
+    fun `completion of hello command when only word is hel`() {
+        val candidates = completer.complete("hel")
+        assertk.assert(candidates).containsAll("hello")
+    }
+
+    @Test
+    fun `completion of sub commands of hello are shown`() {
+        val candidates = completer.complete("hello  ")
+        assertk.assert(candidates).containsAll("say", "count", "invalid")
+    }
+
+    @Test
+    fun `completion of main command's (hello) only option is shown as second word indicates an option`() {
+        val candidates = completer.complete("hello --")
+        assertk.assert(candidates).containsAll("--name", "-n")
+    }
+
+    @Test
+    fun `completion of sub commands options when third word is empty`() {
+        val candidates = completer.complete("hello count  ")
+        assertk.assert(candidates).containsAll("--name", "-n", "--times")
+    }
+
+    @Test
+    fun `completion of sub commands options when third word indicates an option`() {
+        val candidates = completer.complete("hello count -")
+        assertk.assert(candidates).containsAll("--name", "-n", "--times")
+    }
+
+    @Test
+    fun `completion of sub commands options when third word indicates a long option`() {
+        val candidates = completer.complete("hello count --")
+        assertk.assert(candidates).containsAll("--name", "--times")
+    }
+
+    @Test
+    fun `completion of sub commands option 'times'`() {
+        val candidates = completer.complete("hello count --tim")
+        assertk.assert(candidates).containsAll("--times")
+    }
+}
